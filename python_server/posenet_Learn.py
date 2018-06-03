f81483a0eb433efdc70e4e2a1ee179fb74ccbe83
Yuuki S7K <s7k.yuuki@gmail.com>
2018-06-03 20:23:36 +0200

    Puta mierda

--- python_server/posenet_Learn.py
+++ python_server/posenet_Learn.py
@@ -126,9 +126,10 @@ class posenet_learn(object):
                 resultado = self.errorAlcanzado(yAux[k], k)
                 if resultado == False: break
             errorAlcanzado = resultado
-            if i == 10000: errorAlcanzado = True  # En el caso de que se hagan 100000 iteraciones se cierra el bucle
+            if i == 10000:
+                errorAlcanzado = True  # En el caso de que se hagan 100000 iteraciones se cierra el bucle
+                break
             print("Iteraciones: %d" % i)
-            break
         print(self.weights)
 
     def fit(self):
