111d976c2d52777971fe738e79292087957b11d3
Yuuki S7K <s7k.yuuki@gmail.com>
2018-06-03 18:25:11 +0200

    Test1

--- python_server/main.py
+++ python_server/main.py
@@ -14,7 +14,7 @@ class Server(WebSocket):
 
         global entrenar
         if entrenar and len(buffer) < 10:
-            buffer.append(self.data)
+            buffer.append(self.data['position'])
 
         if entrenar and len(buffer) == 10:
             # pasar el buffer a la red
